dd39bf79ef29da3951e47436549a02cdc3300bd9
Lilik Triyawan <43876832+liliktryawan17@users.noreply.github.com>
2022-07-17 13:30:16 +0700

    Update settings.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'form_modelstyling',
+        'NAME': 'django_project',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
